b3acd254595764b762c50cf79bde472356cfb6ce
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-24 15:48:43 +0430

    blog app

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from pages.views import homePageView,aboutpageview
+from pages.views import homePageView,aboutpageview,blogDetail
 urlpatterns = [
     path('', homePageView.as_view(), name = 'home'),
-    path('about', aboutpageview.as_view(), name = 'about')
+    path('about', aboutpageview.as_view(), name = 'about'),
+    path('detail/<int:pk>/',blogDetail.as_view(),name = 'detail')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView ,ListView
-from posts.models import post
+from django.views.generic import TemplateView ,ListView,DetailView
+from blog.models import blog
 # Create your views here.
 # from django.http import HttpResponse
 # def homePageView(request):
 #     return HttpResponse('Hello, World!')
 
 class homePageView(ListView):
-    model = post
+    model = blog
     template_name = 'pages/home.html'
     context_object_name = 'all_posts_list'
 
 class aboutpageview(TemplateView):
     template_name = 'pages/about.html'
     
+class blogDetail(DetailView):
+    model = blog
+    template_name = 'pages/detail.html'
+    context_object_name = 'Detail_post'
